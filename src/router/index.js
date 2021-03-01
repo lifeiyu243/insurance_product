f7cf14133ea04fa2cf9d93bfac41930c867c7fe8
unknown <feiyul@corp.longshine.com>
2021-03-01 10:56:51 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/register')
+    },
+    {
+      path: '/',
+      name: 'index',
+      redirect: '/home',
+      component: () => import('@/views/index.vue'),
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: () => import('@/views/home')
+        }
+      ],
+    }
+  ]
+})
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <m-head />
+        <router-view />
+        <m-foot />
+    </div>
+</template>
+<script>
+import MHead from '@/components/header.vue'
+import MFoot from '@/components/footer.vue'
+export default {
+    components:{
+        MHead,
+        MFoot
+    }
+}
+</script>
